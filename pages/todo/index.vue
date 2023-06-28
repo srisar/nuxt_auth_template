ee8485104bd742dfded0277066669c61c2279eea
Srisaravana M <saravanamuthaly@gmail.com>
2023-06-28 18:24:21 +0530

    Added 'Todo' page in Vue with utilized Pinia store. The todos list is fetched asynchronously from the todos store when the component mounts. The authentication middleware is also applied so that only authenticated users can view todos. Additionally, an empty template was set up for individual todo items.

---
+++ pages/todo/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useTodosStore } from '~/stores/todos';
+import { storeToRefs } from 'pinia';
+
+/* ---------------------------------------------------------------------------------------------- */
+
+definePageMeta({
+  middleware: ['auth'],
+});
+
+/* ---------------------------------------------------------------------------------------------- */
+
+const store = useTodosStore();
+
+const { todos } = storeToRefs(store);
+
+await store.fetchTodos();
+</script>
+
+<template>
+  <h2 class="text-2xl font-bold mb-5">Todos</h2>
+
+  <div v-for="todo in todos">
+    <p :class="[todo.completed ? 'line-through text-primary/20' : '']">{{ todo.title }}</p>
+  </div>
+</template>
+
+<style scoped></style>
