2a1da6640ffa223202dba4df1e8104ddd83cae48
Srisaravana M <saravanamuthaly@gmail.com>
2023-06-29 10:45:43 +0530

    Tested aws-lambda deployment strategy.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,10 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  // nitro: {
+  //   preset: 'aws-lambda'
+  // },
+  // app: {
+  //   cdnURL: 'https://d2g41s5r51ibtk.cloudfront.net',
+  // }
 });
